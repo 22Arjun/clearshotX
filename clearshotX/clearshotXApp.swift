86accc3792eedddc4ea4da5de66bdaf563059e82
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2026-07-03 10:51:00 +0530

    Initial Commit

---
+++ clearshotX/clearshotXApp.swift
@@ -0,0 +1,17 @@
+//
+//  clearshotXApp.swift
+//  clearshotX
+//
+//  Created by Arjun on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct clearshotXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
